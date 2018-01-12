03497bee3a9700ab74e2652daad69bb48f8735cf
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2018-01-12 16:14:10 +0100

    ## v1.0.53: * mitigation of Meltdown / Spectre

--- build_boxes.sh
+++ build_boxes.sh
@@ -1,37 +1,37 @@
 #!/bin/bash -eux
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.52 \
+  --atlas_version=1.0.53 \
   --os=debian \
   --os_version=9.3.0 \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.52 \
+  --atlas_version=1.0.53 \
   --os=debian \
   --os_version=8.10.0 \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.52 \
+  --atlas_version=1.0.53 \
   --os=ubuntu \
   --os_version=14.04.4-server \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.52 \
+  --atlas_version=1.0.53 \
   --os=ubuntu \
   --os_version=16.04.3-server \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.52 \
+  --atlas_version=1.0.53 \
   --os=ubuntu \
   --os_version=17.04-server \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.52 \
+  --atlas_version=1.0.53 \
   --os=ubuntu \
   --os_version=17.10-server \
   --providers=virtualbox,vmware
