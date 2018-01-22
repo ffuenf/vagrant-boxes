6347928580c1659240f773e90b4ebf06edae5724
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2018-01-22 11:03:27 +0100

    * upgrade VirtualBox Guest Additions 5.2.6

--- build_boxes.sh
+++ build_boxes.sh
@@ -1,37 +1,37 @@
 #!/bin/bash -eux
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.53 \
+  --atlas_version=1.0.54 \
   --os=debian \
   --os_version=9.3.0 \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.53 \
+  --atlas_version=1.0.54 \
   --os=debian \
   --os_version=8.10.0 \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.53 \
+  --atlas_version=1.0.54 \
   --os=ubuntu \
   --os_version=14.04.4-server \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.53 \
+  --atlas_version=1.0.54 \
   --os=ubuntu \
   --os_version=16.04.3-server \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.53 \
+  --atlas_version=1.0.54 \
   --os=ubuntu \
   --os_version=17.04-server \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.53 \
+  --atlas_version=1.0.54 \
   --os=ubuntu \
   --os_version=17.10-server \
   --providers=virtualbox,vmware
