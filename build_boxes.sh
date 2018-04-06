0a90eb2920de8bd46d6e81167ed1f47c6eca2e92
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2018-04-06 17:36:27 +0200

    Dev (#65)
    * * upgrade Chef 12.16.42-1
    * upgrade VMware Tools
    * upgrade Rubygems 2.6.8
    * move checksum logic to new packer post-processor
    * add manifests via new packer post-processor
    * completely abandon parallels (cli commands only works with pro or business versions)
    
    * * completely abandon parallels (cli commands only works with pro or business versions)
    
    * fix packer on travis
    
    * * upgrade Chef 12.17.44-1
    * depreciate Ubuntu Wily Werewolf 15.10 Server x86_64
    
    * fix travis
    
    * upgrade VirtualBox Guest Additions 5.1.12
    
    * * added Debian Jessie 8.7.1 x86_64
    * depreciate Debian Jessie 8.6.0 x86_64
    * upgrade Chef 12.18.31-1
    * upgrade Rubygems 2.6.10
    * upgrade VirtualBox Guest Additions 5.1.14
    
    * fix travis
    update packer
    
    * fix travis
    
    * * added Ubuntu Xenial Xerus 16.04.2 Server x86_64
    * depreciate Ubuntu Xenial Xerus 16.04.1 Server x86_64
    
    * try to fix travis
    update rum
    update packer
    style fix in readme
    
    * try to fix travis
    
    * try to fix travis
    
    * try to fix travis
    
    * try to fix travis
    
    * try to fix travis
    
    * try to fix travis
    
    * try to fix travis
    
    * try to fix travis
    
    * try to fix travis
    
    * try to fix travis
    
    * ## v1.0.47:
    * added Debian Stretch 9.1.0 x86_64
    * upgrade Debian Jessie 8.9.0 x86_64
    * upgrade Ubuntu Xenial Xerus 16.04.3 Server x86_64
    * upgrade VirtualBox Guest Additions 5.1.28
    * upgrade Chef 13.4.24-1
    
    * update references to vagrantcloud
    
    * * upgrade Debian Stretch 9.2.1 x86_64
    * depreciate Ubuntu Yakkety Yak 16.10 Server x86_64
    * added Ubuntu Artful Aardvark 17.10 Server x86_64
    * upgrade VirtualBox Guest Additions 5.2.1-118447
    
    * * upgrade VirtualBox Guest Additions 5.2.2
    * upgrade Rubygems 2.7.3
    * upgrade Chef 13.6.4-1
    * upgrade packer 1.1.2
    
    * * added Debian Jessie 8.10.0 x86_64
    * depreciate Debian Jessie 8.9.0 x86_64
    * added Debian Stretch 9.3.0 x86_64
    * depreciate Debian Stretch 9.2.1 x86_64
    * depreciate Debian Stretch 9.1.0 x86_64
    * upgrade Rubygems 2.7.3
    
    * ## v1.0.52:
    * upgrade VirtualBox Guest Additions 5.2.4
    * upgrade VMware Tools 10.2.0 build-7259539
    * upgrade Rubygems 2.7.4
    
    * ## v1.0.53:
    * mitigation of Meltdown / Spectre
    
    * * upgrade VirtualBox Guest Additions 5.2.6
    
    * * remove Ubuntu Zesty Zapus 17.04 Server x86_64
    * update Ubuntu Artful Aardvark 17.10.1 Server x86_64
    * upgrade Rubygems 2.7.5
    * upgrade Chef 13.7.16-1
    
    * * upgrade VirtualBox Guest Additions 5.2.8
    * upgrade Ubuntu Xenial Xerus 16.04.4 Server x86_64
    * upgrade Chef 13.8.3-1
    
    * fix version
    
    * * added Debian Stretch 9.4.0 x86_64
    * depreciate Debian Stretch 9.3.0 x86_64
    * upgrade Rubygems 2.7.6
    * upgrade Chef 14.0.190-1

--- build_boxes.sh
+++ build_boxes.sh
@@ -1,31 +1,31 @@
 #!/bin/bash -eux
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.56 \
+  --atlas_version=1.0.57 \
   --os=debian \
-  --os_version=9.3.0 \
+  --os_version=9.4.0 \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.56 \
+  --atlas_version=1.0.57 \
   --os=debian \
   --os_version=8.10.0 \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.56 \
+  --atlas_version=1.0.57 \
   --os=ubuntu \
   --os_version=14.04.4-server \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.56 \
+  --atlas_version=1.0.57 \
   --os=ubuntu \
   --os_version=16.04.4-server \
   --providers=virtualbox,vmware
 
 bundle exec thor packer:build \
-  --atlas_version=1.0.56 \
+  --atlas_version=1.0.57 \
   --os=ubuntu \
   --os_version=17.10.1-server \
   --providers=virtualbox,vmware
